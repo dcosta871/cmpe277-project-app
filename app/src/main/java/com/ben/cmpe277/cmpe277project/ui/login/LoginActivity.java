604528578e2d43a6909e12355b67bd620dbc85c4
clement55688 <a2040404@gmail.com>
2019-11-02 11:20:56 -0700

    add login and register activities

--- app/src/main/java/com/ben/cmpe277/cmpe277project/ui/login/LoginActivity.java
+++ app/src/main/java/com/ben/cmpe277/cmpe277project/ui/login/LoginActivity.java
@@ -22,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         mTextUsername = (EditText) findViewById(R.id.edittext_username);
         mTextPassword = (EditText) findViewById(R.id.edittext_password);
